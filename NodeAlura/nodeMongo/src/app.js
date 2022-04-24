0f4c557db1bd66626434a705e5660f0730287313
Aritana Noara <arinoara@yahoo.com.br>
2022-04-24 18:58:41 -0300

    Crud

---
+++ NodeAlura/nodeMongo/src/app.js
@@ -0,0 +1,61 @@
+import express from 'express';
+
+const app = express();
+
+//para interpretar o que chega via post/put
+app.use(express.json());
+
+const books = [
+    { id: 1, "title": "The Hobbit" },
+    { id: 2, "title": "Pirates of the Asteroids" },
+];
+
+app.get('/', (req, res) => {
+    res.status(200).send('Curso de Node')
+});
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(books)
+})
+
+app.get('/livros/:id', (req, res) => {
+    let index = bookSearch(req.params.id);
+    res.status(200);
+    res.json(books[index]);
+})
+
+app.post('/livros', (req, res) => {
+
+    let nextIndex = getNextId()
+    let book = { id: nextIndex, "title": req.body.title };
+    books.push(book);
+    res.status(201).send('Livro cadastrado com sucesso');
+});
+
+app.put('/livros/:id', (req, res) => {
+    let index = bookSearch(req.params.id);
+    books[index].title = req.body.title;
+    res.status(201);
+    res.json(books);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let{id} = req.params;
+    let index = bookSearch(id);
+    books.splice(index,1);
+    res.status(200);
+    res.send(`Livro ${id} removido com sucesso`);
+})
+
+
+function bookSearch(id) {
+    return books.findIndex(book => book.id == id)
+}
+
+function getNextId() {
+    return books.length + 1
+}
+
+
+export default app;
+
