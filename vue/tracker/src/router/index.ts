fffb65ea4bd4cec235c4316c4da01d7ea1efeb75
Aritana Noara <arinoara@yahoo.com.br>
2022-04-24 22:40:24 -0300

    git states

--- vue/tracker/src/router/index.ts
+++ vue/tracker/src/router/index.ts
@@ -1,18 +1,39 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 import Tarefas from '../views/MinhasTarefas.vue'
 import Projetos from '../views/MeusProjetos.vue'
+import Formulario from '../views/projects/MeuFormulario.vue'
+import Lista from '../views/projects/MinhaLista.vue'
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'Tarefas',
         component: Tarefas
-    },
+    },  
     {
         path: '/projetos',
-        name: 'Projetos',
-        component: Projetos
+        component: Projetos,
+        children: [
+
+            {
+                path: '',
+                name: 'Projetos',
+                component: Lista
+            },
+            {
+                path: 'novo',
+                name: 'Novo projeto',
+                component: Formulario
+            },
+            {
+                path: ':id',
+                name: 'Editar projeto',
+                component: Formulario,
+                props: true //injeta como uma props
+            }
+        ]
     }
+
 ];
 const router = createRouter({
     history: createWebHashHistory(),
--- vue/tracker/src/store/index.ts
+++ vue/tracker/src/store/index.ts
@@ -1,6 +1,6 @@
 import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
-import { createStore, Store,useStore as vuexUseStore } from "vuex";
+import { createStore, Store, useStore as vuexUseStore } from "vuex";
 
 
 
@@ -23,10 +23,18 @@ export const store = createStore<Estado>({
                 nome: nomeDoProjeto
             } as IProjeto
             state.projetos.push(projeto)
+        },
+        'ALTERA_PROJETO'(state, projeto: IProjeto) {
+            const index = state.projetos.findIndex(proj => proj.id == projeto.id)
+            state.projetos[index] = projeto
+        },
+        "EXCLUIR_PROJETO"(state, id: string) { //sobrepor
+            state.projetos = state.projetos.filter(proj => proj.id != id)
+
         }
     }
 })
 
- export function useStore(): Store<Estado> {
-     return vuexUseStore(key)
- }
+export function useStore(): Store<Estado> {
+    return vuexUseStore(key)
+}
