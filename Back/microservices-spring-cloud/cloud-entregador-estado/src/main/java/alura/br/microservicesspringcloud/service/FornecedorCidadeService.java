4d2b4848cefe470ae5c3238ecf40f544a3620db3
Aritana Noara <arinoara@yahoo.com.br>
2022-06-26 16:57:37 -0300

    setting exception to catch conection failure between services stado and cidade

--- Back/microservices-spring-cloud/cloud-entregador-estado/src/main/java/alura/br/microservicesspringcloud/service/FornecedorCidadeService.java
+++ Back/microservices-spring-cloud/cloud-entregador-estado/src/main/java/alura/br/microservicesspringcloud/service/FornecedorCidadeService.java
@@ -1,6 +1,7 @@
 package alura.br.microservicesspringcloud.service;
 
 import alura.br.microservicesspringcloud.dto.InfoFornecedorCidadeDto;
+import alura.br.microservicesspringcloud.exception.ServerErrorException;
 import alura.br.microservicesspringcloud.networking.service.FornecedorCidadeServiceCore;
 import org.apache.logging.slf4j.SLF4JLogger;
 import org.slf4j.Logger;
@@ -21,8 +22,15 @@ public class FornecedorCidadeService {
 
     public InfoFornecedorCidadeDto getDados(String cidade) {
         logger.info("Buscando informações do fornecedor de {}", cidade);
+        try{
+            InfoFornecedorCidadeDto infoFornecedorCidadeDto = fornecedorCidadeServiceCore.getFornecedor(cidade);
+            return infoFornecedorCidadeDto;
+        }catch (feign.RetryableException exception) {
+            UnsupportedOperationException unsupportedOperationException = new UnsupportedOperationException("Feign failure");
+            unsupportedOperationException.initCause(exception);
+            ServerErrorException serverErrorException = new ServerErrorException("Falha Interna", unsupportedOperationException);
+            throw serverErrorException;
+        }
 
-        InfoFornecedorCidadeDto infoFornecedorCidadeDto = fornecedorCidadeServiceCore.getFornecedor(cidade);
-        return infoFornecedorCidadeDto;
     }
 }
